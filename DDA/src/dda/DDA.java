abd4a672cf9a3c7d34191ba9ad38c3a24c566d24
Mohamed <moaw769@gmail.com>
2021-04-20 04:37:21 +0200

    add file of java program

---
+++ DDA/src/dda/DDA.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package dda;
+
+import java.awt.Graphics;
+
+/**
+ *
+ * @author abdalrahman
+ */
+public class DDA extends javax.swing.JPanel{
+public DDA(){}
+  
+
+
+  
+
+   
+    
+    
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        DDAF d=new DDAF();
+        d.setVisible(true);
+
+    }
+    
+}
